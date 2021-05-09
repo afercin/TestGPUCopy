25b48a823c0b29790836336b56d51b9dd53110df
afercin <afercin@gmail.com>
2021-05-09 11:36:48 +0200

    Agregar archivos de proyecto.

---
+++ TestGPUCopy/GPU.cs
@@ -0,0 +1,58 @@
+﻿using Cloo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGPUCopy
+{
+    class GPU
+    {
+        private ComputeCommandQueue queue;
+        private ComputeContext context;
+        private ComputeKernel kernel;
+
+        private string ArrayCopy
+        {
+            get
+            {
+                return @"
+                        kernel void ArrayCopy(global uchar* bufferIn, int inOffset, global uchar* bufferOut, int outOffset) 
+                        {
+                              int index = get_global_id(0);
+                              bufferOut[index + outOffset] = bufferIn[index + inOffset];
+                        }";
+            }
+        }
+
+        public GPU()
+        {
+            context = new ComputeContext(ComputeDeviceTypes.Gpu, new ComputeContextPropertyList(ComputePlatform.Platforms[0]), null, IntPtr.Zero);
+            queue = new ComputeCommandQueue(context, context.Devices[0], ComputeCommandQueueFlags.None);
+            var program = new ComputeProgram(context, ArrayCopy);
+            program.Build(null, null, null, IntPtr.Zero);
+
+            kernel = program.CreateKernel("ArrayCopy");
+        }
+
+        public void GPUCopy(byte[] origin, int originOffset, byte[] dest, int destOffset, int lenght)
+        {
+
+            ComputeBuffer<byte> originBuffer, destBuffer;
+
+            originBuffer = new ComputeBuffer<byte>(context,
+                ComputeMemoryFlags.ReadOnly | ComputeMemoryFlags.UseHostPointer, origin);
+            destBuffer = new ComputeBuffer<byte>(context,
+                ComputeMemoryFlags.ReadOnly | ComputeMemoryFlags.UseHostPointer, dest);
+
+            kernel.SetMemoryArgument(0, originBuffer);
+            kernel.SetValueArgument(1, originOffset);
+            kernel.SetMemoryArgument(2, destBuffer);
+            kernel.SetValueArgument(3, destOffset);
+
+            queue.Execute(kernel, new long[] { 0 }, new long[] { lenght }, null, new ComputeEventList());
+            queue.Finish();
+        }
+    }
+}
---
+++ TestGPUCopy/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestGPUCopy
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GPU gpu;
+        public MainWindow()
+        {
+            InitializeComponent();
+            gpu = new GPU();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] data = ImageToBytes(Image.FromFile(@"D:/prueba.png"));
+            byte[] copy = new byte[data.Length];
+
+            gpu.GPUCopy(data, 0, copy, 0, data.Length);
+
+            Img.Source = BytesToScreenImage(copy);
+        }
+        public static byte[] ImageToBytes(Image img)
+        {
+            byte[] data;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, ImageFormat.Jpeg);
+                data = ms.ToArray();
+            }
+            return data;
+        }
+
+
+        public static BitmapImage BytesToScreenImage(byte[] imageBytes)
+        {
+            BitmapImage bitmap = new BitmapImage();
+            using (MemoryStream stream = new MemoryStream(imageBytes))
+            {
+                bitmap.BeginInit();
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.StreamSource = stream;
+                bitmap.EndInit();
+            }
+            return bitmap;
+        }
+    }
+}
